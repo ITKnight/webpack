3c9b08489e4fc400094493270a194d1ae89e727a
ITKNIGHT <598940445@qq.com>
2017-02-18 11:42:45 +0800

    fix node error of prompts

--- meta.js
+++ meta.js
@@ -23,8 +23,9 @@ module.exports = {
     "author": {
       "type": "string",
       "message": "Author"
-    },
+    }
+  },
 
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://github.com/itknight/webpack"
-}
+
 }
