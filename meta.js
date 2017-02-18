30c98235a124217ae5bf86ec17f87539cff5a8ed
ITKNIGHT <598940445@qq.com>
2017-02-18 11:41:52 +0800

    remove filters node

--- meta.js
+++ meta.js
@@ -25,8 +25,6 @@ module.exports = {
       "message": "Author"
     },
 
-
-  "filters": { },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://github.com/itknight/webpack"
 }
 }
