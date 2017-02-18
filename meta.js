0bd9b31caf5fa3cc142e032d40671374208757e6
ITKNIGHT <598940445@qq.com>
2017-02-18 11:40:05 +0800

    fix syntax error of ;

--- meta.js
+++ meta.js
@@ -28,4 +28,5 @@ module.exports = {
 
   "filters": { },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://github.com/itknight/webpack"
-};
+}
+}
